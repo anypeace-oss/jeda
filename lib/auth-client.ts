3a83e30ad9d67d9336248f1df5871a1653d3b6a9
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-07-24 20:19:24 +0700

    Update authentication client to use BETTER_AUTH_URL for base URL configuration.

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   cookiePrefix: "ba_", // match your server config if customized
 });
